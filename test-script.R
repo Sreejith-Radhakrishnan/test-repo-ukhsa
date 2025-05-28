1b21345ae71719d8cd987a727149bf14d2fb144e
Sreejith Radhakrishnan <sreejith.radhakrishnan@ukhsa.gov.uk>
2025-05-28 14:24:14 +0100

    pushing first change

--- test-script.R
+++ test-script.R
@@ -1,1 +1,5 @@
 #testing pushing a local project to Github
+#Note, when creating a local project, and then pushing to github when a repo for this project doesn't already exist, the best 
+#thing to do is to use the followign command when you're in your project directory
+
+usethis::use_github()
