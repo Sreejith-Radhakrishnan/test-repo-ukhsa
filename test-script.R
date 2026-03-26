220a9a9fe520587a3ddf1ddd75150a284e8ef24d
Sreejith Radhakrishnan <sreejith.radhakrishnan@ukhsa.gov.uk>
2026-03-26 11:11:00 +0000

    A commit from my local computer

--- test-script.R
+++ test-script.R
@@ -2,4 +2,4 @@
 #Note, when creating a local project, and then pushing to github when a repo for this project doesn't already exist, the best 
 #thing to do is to use the followign command when you're in your project directory
 
-usethis::use_github()
+usethis::use_github()"adding a line" 
